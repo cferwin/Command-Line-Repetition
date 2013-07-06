a81a57d8aa4a17e8b41fd09af1c3d4c298421d94
Christopher Erwin <chrisferwin@gmail.com>
2013-07-06 13:06:40 -0700

    Add Collection class

---
+++ collection.py
@@ -0,0 +1,26 @@
+import random
+
+class Collection:
+    """ A collection of information to be practiced. """
+
+    def __init__(self, name):
+        self.name = name
+        self.slides = []
+
+    def add_slide(self, slide):
+        """ Add a slide to the collection. """
+        self.slides.append(slide)
+
+    def remove_slide(self, slide):
+        """ Remove a slide from the collection. """
+        self.slides.remove(slide)
+
+    def get_random_slides(self, n, rand=random):
+        """ Return a list of randomly selected slides.
+
+            Variables:
+            n       int     Number of slides to return
+            rand    object  Source for randomness. Defaults to "random".
+        """
+
+        return rand.sample(self.slides, n)
