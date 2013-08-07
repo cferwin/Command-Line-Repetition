6088764afd67320f83e035ca07a668480c7a15be
Christopher Erwin <chrisferwin@gmail.com>
2013-08-06 22:46:09 -0700

    Add support for navigation menus and dialogue menus.
    Add: router_menu() to Menu.
    Add: DialogueMenu for representing menus with multiple Edit dialogue boxes.
    Add: new_collection() to Menu.

--- menu.py
+++ menu.py
@@ -2,12 +2,18 @@
 """
 
 import urwid
+from collection import Collection
 
 class Menu():
     def __init__(self, collections):
         self.collections = collections
+        options = [
+            ('New Collection', self.new_collection),
+            ('Study Collection', self.choose_collection),
+            ('Edit Collection', self.choose_collection)]
+
         self.main = urwid.Padding(
-            self.selection_menu(u'Spaced Repetition', collections, self.choose_slide),
+            self.router_menu(u'Spaced Repetition', options),
             left = 2, right = 2)
 
         self.top = urwid.Overlay(self.main,
@@ -17,19 +23,30 @@ class Menu():
             min_width = 20, min_height = 9)
 
     def run(self):
+        """ Runs the menu system """
         urwid.MainLoop(self.top, palette=[('reversed', 'standout', '')]).run()
 
-    def exit_program(self, button):
+    def exit(self, button):
+        """ Exits the menu system """
         raise urwid.ExitMainLoop()
 
     # ----------------------------------------
     # Specific menu screens
 
+    def main(self):
+        options = [
+            ('New Collection', self.new_collection),
+            ('Study Collection', self.choose_collection),
+            ('Edit Collection', self.choose_collection)]
+
+        self.main.original_widget = urwid.Padding(
+            self.router_menu(u'Spaced Repetition', options))
+
     def item_chosen(self, button, choice):
         response = urwid.Text([u'You chose ', str(choice), u'\n'])
         done = urwid.Button(u'Ok')
 
-        urwid.connect_signal(done, 'click', self.exit_program)
+        urwid.connect_signal(done, 'click', self.exit)
         self.main.original_widget = urwid.Filler(urwid.Pile([
             response, urwid.AttrMap(done, None, focus_map='reversed')]))
 
@@ -47,6 +64,30 @@ class Menu():
                 self.collections,
                 self.choose_slide))
 
+    def new_collection(self, button):
+        """ Render the menu for creating a new Collection """
+
+        def do_new_collection(button, dialogue_menu):
+            """ Create a new collection with the data from the
+                'new_collection' menu.
+            """
+
+            # TODO: Rewrite this function to create a new Collection. This is
+            # just a stand-in.
+            fields = dialogue_menu.get_fields()
+
+            self.item_chosen(button,
+                "\nName: " + fields['Name']
+                + "\nDescription: " + fields['Description'])
+
+        menu = DialogueMenu(
+                'Choose a collection',
+                ['Name', 'Description'],
+                do_new_collection,
+                self.main)
+
+        self.main.original_widget = urwid.Padding(menu.get_widget())
+
     # ----------------------------------------
     # Constructors for menu structures
 
@@ -54,9 +95,71 @@ class Menu():
         body = [urwid.Text(title), urwid.Divider()]
 
         for c in choices:
-            button = urwid.Button(str(c))
+            button = urwid.Button(str(c), callback, c)
 
-            urwid.connect_signal(button, 'click', callback, c)
             body.append(urwid.AttrMap(button, None, focus_map='reversed'))
 
         return urwid.ListBox(urwid.SimpleFocusListWalker(body))
+
+    def router_menu(self, title, choices):
+        body = [urwid.Text(title), urwid.Divider()]
+
+        for c in choices:
+            button = urwid.Button(str(c[0]), c[1])
+
+            body.append(urwid.AttrMap(button, None, focus_map='reversed'))
+
+        return urwid.ListBox(urwid.SimpleFocusListWalker(body))
+
+class DialogueMenu:
+    # TODO: Turn this class into a functional Urwid Widget?
+
+    def __init__(self, title, field_labels, forward, back):
+        """ Variables:
+            title           string      Title of the menu.
+            field_labels    array       Array of strings -- labels for the
+                                        edit widgets which make up the menu.
+            forward         function    Called when 'OK' button is pressed.
+            back            function    Called when 'Cancel' button is
+                                        pressed.
+        """
+
+        self._field_widgets = []
+
+        self.body = [urwid.Text(title), urwid.Divider()]
+        ok = urwid.Button("OK", forward, self)
+        cancel = urwid.Button("Cancel", back, self)
+
+        # Add Edit widgets (fields)
+        for field in field_labels:
+            widget = self._dialogue_box(field)
+
+            self.body.append(urwid.AttrMap(
+                widget, None, focus_map='reversed'))
+            self._field_widgets.append(widget)
+
+        # Add OK and Cancel buttons
+        self.body.append(urwid.AttrMap(ok, None, focus_map='reversed'))
+        self.body.append(urwid.AttrMap(cancel, None, focus_map='reversed'))
+
+    def get_widget(self):
+        """ Returns a renderable widget representing the Dialogue Menu. """
+        return urwid.ListBox(urwid.SimpleFocusListWalker(self.body))
+
+    def get_fields(self):
+        """ Returns the contents of all the dialogue boxes in a dictionary
+            with the form, {"label", "contents"}
+        """
+
+        fields = {}
+        for widget in self._field_widgets:
+            label = widget.caption
+            contents = widget.get_edit_text()
+            fields[label] = contents
+
+        return fields
+
+    def _dialogue_box(self, query):
+        # Constructs a widget for a dialogue box.
+        return urwid.Edit(('I say', query))
+
